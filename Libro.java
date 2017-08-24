01bc16cfda35bfffa67b4ba373f04a76b2c323ca
omar navarro <omm27@hotmail.com>
2017-08-24 10:31:36 -0500

    Se tiene que capturar los datos con comillas, ya que son cadenas.

---
+++ Libro.java
@@ -0,0 +1,15 @@
+public class Libro
+{
+    //variables de instancia
+    private String titulo;
+    private String autor;
+    
+    //constructor
+    public Libro(String tit,String aut)
+    {
+        titulo=tit;
+        autor=aut;
+    }
+    
+  
+}
