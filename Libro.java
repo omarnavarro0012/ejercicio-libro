d85df6b91c64c70f5160e897fde0ce4c316e8986
omar navarro <omm27@hotmail.com>
2017-08-24 10:56:33 -0500

    se agrego un metodo que da todos los detalles en general y que se regresa por un return , mas que la variable temporal es una cadena que se utilizo para poderlo mandar como mensaje.

--- Libro.java
+++ Libro.java
@@ -3,12 +3,14 @@ public class Libro
     //variables de instancia
     private String titulo;
     private String autor;
+    private int paginas;
     
     //constructor
-    public Libro(String tit,String aut)
+    public Libro(String tit,String aut,int pag)
     {
         titulo=tit;
         autor=aut;
+        paginas=pag;
     }
     //metodos   
     
@@ -22,5 +24,25 @@ public class Libro
    {
        return titulo;
    }
+   
+   public int cuantasPaginas()
+   {
+       return paginas;
+    }
   
+   public String DetallesGen()
+   {
+       String detalles="";
+       detalles+="Titulo: ";
+       detalles+=titulo;
+       detalles+=" ,";
+       detalles+="Autor: ";
+       detalles+=autor;
+       detalles+=" ,";
+       detalles+="Paginas: ";
+       detalles+=paginas;
+       detalles+=".";
+       
+       return detalles;
+    }
 }
